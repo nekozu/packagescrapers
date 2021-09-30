d93b367d4c292d4673e2324994fa522fcc15dc9e
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-30 10:40:23 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 const pack = require('./index.js')
 
 pack.github('telegram').then(res => console.log(res))
+
+pack.npm('telegram').then(res => console.log(res))
+
+pack.pypi('telegram').then(res => console.log(res))
